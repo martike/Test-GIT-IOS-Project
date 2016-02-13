6cfd7289806f1cbbfd81af3a60336ea4fe2bfc84
Ken Martin <kmartin@Kens-MacBook-Pro-2.local>
2016-02-12 16:39:45 -0800

    added a test class

---
+++ GitTest/KMTest.h
@@ -0,0 +1,13 @@
+//
+//  KMTest.h
+//  GitTest
+//
+//  Created by Ken Martin on 2/12/16.
+//  Copyright (c) 2016 Ken Martin. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface KMTest : NSObject
+
+@end
---
+++ GitTest/KMTest.m
@@ -0,0 +1,13 @@
+//
+//  KMTest.m
+//  GitTest
+//
+//  Created by Ken Martin on 2/12/16.
+//  Copyright (c) 2016 Ken Martin. All rights reserved.
+//
+
+#import "KMTest.h"
+
+@implementation KMTest
+
+@end
